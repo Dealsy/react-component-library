4381cabe1ae4679442e3046275f715f3ce50fe14
Matt Deal <contact@mattdeal.com.au>
2023-08-03 11:19:56 +1000

    updates button styles

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,7 +4,7 @@ import { ButtonProps } from "@/types/buttonTypes";
 const filled =
   "bg-green-600 text-white hover:scale-110 hover:bg-green-500 active:bg-green-600";
 const disabledStyle =
-  "opacity-50 bg-gray-300 text-gray-500 cursor-default hover:bg-gray-300 hover:scale-110 active:bg-gray-300 active:scale-100";
+  "opacity-50 bg-gray-300 text-gray-500 cursor-default hover:bg-gray-300";
 const text =
   "hover:underline active:underline hover:bg-inherit active:text-lg active:bg-inherit active:scale-100";
 const outlined =
@@ -26,14 +26,14 @@ export function Button({
       disabled={disabled}
       onClick={onClick}
       className={clsx(
-        className && className,
         "py-3 px-5 transition duration-300 ease-in-out transform rounded-full text-sm leading-none",
         "font-medium",
-        buttonStyle === "elevated" && elevated,
-        buttonStyle === "outlined" && outlined,
-        buttonStyle === "text" && text,
-        buttonStyle === "filled" && filled,
-        disabled && disabledStyle
+        buttonStyle === "elevated" && !disabled && elevated,
+        buttonStyle === "outlined" && !disabled && outlined,
+        buttonStyle === "text" && !disabled && text,
+        buttonStyle === "filled" && !disabled && filled,
+        disabled && disabledStyle,
+        className && className
       )}
     >
       <div className="flex items-center justify-center gap-5">
--- src/types/buttonTypes.ts
+++ src/types/buttonTypes.ts
@@ -1,7 +1,7 @@
 export type ButtonProps = {
     onClick?: () => void
     title: string
-    buttonStyle?: 'elevated' | 'outlined' | 'text' | 'filled'
+    buttonStyle?: 'elevated' | 'outlined' | 'text' | 'filled' 
     disabled?: boolean
     leftIcon?: React.ReactNode
     rightIcon?: React.ReactNode
