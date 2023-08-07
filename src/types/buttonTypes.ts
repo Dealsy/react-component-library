2de90e9cea5150f3fbe0952041bb8d9ab5224fcd
Matt Deal <contact@mattdeal.com.au>
2023-08-07 16:11:09 +1000

    Updates types, and adds button Types

--- src/types/buttonTypes.ts
+++ src/types/buttonTypes.ts
@@ -1,11 +1,13 @@
+import { buttonType, classNamesMap, sizes } from "@/components/Button/Button";
+
 export type ButtonProps = {
     onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
     title: string
-    buttonStyle?: 'elevated' | 'outlined' | 'text' | 'filled' 
-    size?: "small" | "medium" | "large";
+    buttonStyle?: keyof typeof classNamesMap;
+    buttonType?: keyof typeof buttonType;
+    size?: keyof typeof sizes;
     disabled?: boolean
     leftIcon?: React.ReactNode
     rightIcon?: React.ReactNode
     className?: string
-    // TODO - Add button types i.e cancel, confirm, these should have default styles
 } 
