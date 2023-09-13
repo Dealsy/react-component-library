a850160637ab7dade31713936c0aa121b7b525f9
Matt Deal <contact@mattdeal.com.au>
2023-09-13 14:20:18 +1000

    Refactored button component

---
+++ src/constants.ts
@@ -0,0 +1,25 @@
+export const sizes = {
+  small: 'small',
+  medium: 'medium',
+  large: 'large',
+} as const
+
+export const buttonType = {
+  submit: 'submit',
+  button: 'button',
+  reset: 'reset',
+} as const
+
+export const styles = {
+  filled: 'filled',
+  filledCancel: 'filledCancel',
+  filledDisabledStyle: 'filledDisabledStyle',
+  text: 'text',
+  textDisabledStyle: 'textDisabledStyle',
+  outlined: 'outlined',
+  outlinedCancel: 'outlinedCancel',
+  outlinedDisabledStyle: 'outlinedDisabledStyle',
+  elevated: 'elevated',
+  elevatedCancel: 'elevatedCancel',
+  elevatedDisabledStyle: 'elevatedDisabledStyle',
+} as const
--- src/types/buttonTypes.ts
+++ src/types/buttonTypes.ts
@@ -1,48 +1,52 @@
-import { buttonType, classNamesMap, sizes } from "@/components/Button/Button";
+import { buttonType, sizes, styles } from '@/constants'
+
+type Size = (typeof sizes)[keyof typeof sizes]
+type ButtonType = (typeof buttonType)[keyof typeof buttonType]
+type ButtonStyle = (typeof styles)[keyof typeof styles]
 
 export type ButtonProps = {
-    /**
-     * A callback function triggered when the button is clicked.
-     */
-    onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  /**
+   * A callback function triggered when the button is clicked.
+   */
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void
 
-    /**
-     * The text or label displayed on the button.
-     */
-    title: string;
+  /**
+   * The text or label displayed on the button.
+   */
+  title: string
 
-    /**
-     * The style class of the button, determined by the classNamesMap.
-     */
-    buttonStyle?: keyof typeof classNamesMap;
+  /**
+   * The style class of the button, determined by the classNamesMap.
+   */
+  buttonStyle?: ButtonStyle
 
-    /**
-     * The type or purpose of the button, such as 'submit' or 'reset'.
-     */
-    buttonType?: keyof typeof buttonType;
+  /**
+   * The type or purpose of the button, such as 'submit' or 'reset'.
+   */
+  buttonType?: ButtonType
 
-    /**
-     * The size of the button, e.g., 'small', 'medium', 'large'.
-     */
-    size?: keyof typeof sizes;
+  /**
+   * The size of the button, e.g., 'small', 'medium', 'large'.
+   */
+  size?: Size
 
-    /**
-     * If true, the button will be disabled and not interactable.
-     */
-    disabled?: boolean;
+  /**
+   * If true, the button will be disabled and not interactable.
+   */
+  disabled?: boolean
 
-    /**
-     * An optional icon to display on the left side of the button text.
-     */
-    leftIcon?: React.ReactNode;
+  /**
+   * An optional icon to display on the left side of the button text.
+   */
+  leftIcon?: React.ReactNode
 
-    /**
-     * An optional icon to display on the right side of the button text.
-     */
-    rightIcon?: React.ReactNode;
+  /**
+   * An optional icon to display on the right side of the button text.
+   */
+  rightIcon?: React.ReactNode
 
-    /**
-     * You may optionally pass in a custom class name to be applied to the button.
-     */
-    className?: string;
-} 
+  /**
+   * You may optionally pass in a custom class name to be applied to the button.
+   */
+  className?: string
+}
